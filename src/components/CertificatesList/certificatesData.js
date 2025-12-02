24693dd7d364a5ce782aed28eb3d04b206efbed5
Carolina <carolinagrizotti5@gmail.com>
2025-12-02 15:39:25 -0300

    teste

--- src/components/CertificatesList/certificatesData.js
+++ src/components/CertificatesList/certificatesData.js
@@ -394,38 +394,10 @@ export const allCertificates = [
           { name: 'cnpj', label: 'CNPJ', type: 'text' },
           { name: 'cei_opcional', label: 'CEI (Opcional)', type: 'text' },
           { 
-            name: 'estado', 
-            label: 'Estado', 
-            type: 'select', 
-            options: [
-              { label: 'AC', value: 'AC' },
-              { label: 'AL', value: 'AL' },
-              { label: 'AP', value: 'AP' },
-              { label: 'AM', value: 'AM' },
-              { label: 'BA', value: 'BA' },
-              { label: 'CE', value: 'CE' },
-              { label: 'DF', value: 'DF' },
-              { label: 'ES', value: 'ES' },
-              { label: 'GO', value: 'GO' },
-              { label: 'MA', value: 'MA' },
-              { label: 'MT', value: 'MT' },
-              { label: 'MS', value: 'MS' },
-              { label: 'MG', value: 'MG' },
-              { label: 'PA', value: 'PA' },
-              { label: 'PB', value: 'PB' },
-              { label: 'PR', value: 'PR' },
-              { label: 'PE', value: 'PE' },
-              { label: 'PI', value: 'PI' },
-              { label: 'RJ', value: 'RJ' },
-              { label: 'RN', value: 'RN' },
-              { label: 'RS', value: 'RS' },
-              { label: 'RO', value: 'RO' },
-              { label: 'RR', value: 'RR' },
-              { label: 'SC', value: 'SC' },
-              { label: 'SP', value: 'SP' },
-              { label: 'SE', value: 'SE' },
-              { label: 'TO', value: 'TO' }
-            ]
+            name: 'estado',
+            label: 'Estado',
+            type: 'select',
+            options: ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
           }
         ]
       }
