101b1b2a3d7667aa30c4d474dc952931b5e65244
Carolina <carolinagrizotti5@gmail.com>
2025-12-02 15:16:24 -0300

    correção

--- src/components/CertificatesList/certificatesData.js
+++ src/components/CertificatesList/certificatesData.js
@@ -387,7 +387,6 @@ export const allCertificates = [
     { id: 9, name: 'Certidão de Débitos Trabalhistas (MT)', esfera: 'Federal', govFormFields: { pessoa: [{ name: 'cpf', label: 'CPF' }], empresa: [{ name: 'cnpj', label: 'CNPJ' }] } },
     { id: 11, name: 'Certidão Negativa do FGTS', esfera: 'Federal', 
       govFormFields: {
-        needsState: true, 
         pessoa: [], 
         empresa: [{ name: 'cnpj', label: 'CNPJ' }, { name: 'cei_opcional', label: 'CEI (Opcional)', required: false }]
       } 
