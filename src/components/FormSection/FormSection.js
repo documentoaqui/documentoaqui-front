bd50e2d5de0891664a0579dc2f193ff1316fd32d
Carolina <carolinagrizotti5@gmail.com>
2025-12-12 19:37:09 -0300

    ajuste na url

--- src/components/FormSection/FormSection.js
+++ src/components/FormSection/FormSection.js
@@ -29,7 +29,7 @@ const handleSubmit = async (e) => {
   const data = Object.fromEntries(formData.entries());
 
   try {
-    const response = await fetch("/api/contato", {
+    const response = await fetch("http://72.61.42.248:3000/api/contato", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
