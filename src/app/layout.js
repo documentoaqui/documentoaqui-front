03d37485626f140ce65a9f16c6a1e0a819024f01
Carolina <carolinagrizotti5@gmail.com>
2025-12-15 08:30:40 -0300

    teste 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        {/* <Script
+        <Script
           id="google-tag-manager"
           strategy="afterInteractive"
         >
@@ -28,18 +28,18 @@ export default function RootLayout({ children }) {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-KDZZB5KZ');
           `}
-        </Script> */}
+        </Script>
       </head>
 
       <body className={montserrat.className}>
-        <noscript>
+        {/* <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KDZZB5KZ"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
-        </noscript>
+        </noscript> */}
 
         <AuthProvider>
           <CartProvider>{children}</CartProvider>
