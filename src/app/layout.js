bd72197220a380d844d93f5ced627c52e4e596c4
Carolina <carolinagrizotti5@gmail.com>
2025-12-15 08:28:05 -0300

    Ajuste tag google

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,38 +17,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        {/* Google Tag Manager */}
-        {/* <Script
-          id="gtm-script"
+        <Script
+          id="google-tag-manager"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KDZZB5KZ');
-            `,
-          }}
-        /> */}
-        {/* End Google Tag Manager */}
+        >
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KDZZB5KZ');
+          `}
+        </Script>
       </head>
 
       <body className={montserrat.className}>
-        {/* Google Tag Manager (noscript) */}
-        {/* <noscript>
+        <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KDZZB5KZ"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
-        </noscript> */}
-        {/* End GTM */}
+        </noscript>
 
         <AuthProvider>
           <CartProvider>{children}</CartProvider>
