c518fc308e2dfe806077a350a6eee27b05974026
Carolina <carolinagrizotti5@gmail.com>
2025-12-15 19:10:22 -0300

    tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,30 +17,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-        >
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KDZZB5KZ');
-          `}
-        </Script>
+        {/* <!-- Google Tag Manager --> */}
+        <script>
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KDZZB5KZ');
+        </script>
+        {/* <!-- End Google Tag Manager --> */}
       </head>
 
       <body className={montserrat.className}>
-        {/* <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KDZZB5KZ"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript> */}
-
+        {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KDZZB5KZ"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+      </noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
         <AuthProvider>
           <CartProvider>{children}</CartProvider>
         </AuthProvider>
