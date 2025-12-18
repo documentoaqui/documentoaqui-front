d4b991859e36884c0e0e0496609c14c17b1dfa48
Carolina <carolinagrizotti5@gmail.com>
2025-12-18 19:07:51 -0300

    teag

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,34 +17,28 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
+         <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXX');
+              })(window,document,'script','dataLayer','GTM-KDZZB5KZ');
             `,
           }}
         />
-        {/* End Google Tag Manager */}
       </head>
 
       <body className={montserrat.className}>
-        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KDZZB5KZ"
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }}
+            style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
 
         <AuthProvider>
           <CartProvider>{children}</CartProvider>
