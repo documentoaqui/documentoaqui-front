53bbb894b7c7d890a004ed68ec01970545fab97e
Carolina <carolinagrizotti5@gmail.com>
2025-12-18 20:34:53 -0300

    teste 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,11 +17,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager - bootstrap */}
         <Script
-          id="gtm-script"
+          id="gtm-bootstrap"
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtm.js?id=GTM-KDZZB5KZ"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KDZZB5KZ');
+            `,
+          }}
         />
       </head>
 
